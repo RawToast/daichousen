d9843d2ebd9e288b8f713eb4d049f53f3faa655d
rawtoast <jim@jamesmorris.org>
2018-03-17 23:27:54 +0000

    Delete more files

--- src/main/scala/daichousen/data/Enemy.scala
+++
@@ -1,9 +0,0 @@
-package daichousen.data
-
-import java.util.UUID
-
-case class Enemy(id: UUID, meta: EnemyMetaData, enemyStats: EnemyStats, positionOffset: Int)
-
-case class EnemyMetaData(name: String)
-
-case class EnemyStats(stats: BasicStats, equipment: Equipment, status: Seq[Status])
--- src/main/scala/daichousen/data/Equipment.scala
+++
@@ -1,9 +0,0 @@
-package daichousen.data
-
-case class Equipment(weapon: Option[Weapon], armour: Option[Armour])
-
-case class Weapon(name: String, dmg: Int, requirements: Requirements, effects: Seq[WeaponEffect])
-
-sealed trait WeaponEffect
-
-case class Armour(name: String, defense: Int, requirements: Requirements)
