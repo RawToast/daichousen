b36226872fb304fdd70658a0bafb96f6c7c67838
rawtoast <jim@jamesmorris.org>
2018-04-07 23:04:14 +0100

    Reduce guide and patch notes * Updated reference to chousen * Set version to 0.0.5

--- frontend/src/app/landing/create.ts
+++ frontend/src/app/landing/create.ts
@@ -8,14 +8,7 @@ import { Router } from '@angular/router';
     template: `<input id ="namebox" type="text" class="form-control" placeholder="Character Name" (keyup)="onKey($event)">
                 <br>
                 <fieldset>
-                    <input type="radio" name="charc" value="2" (click)="classChoice(1)" checked> Fighter
-                    <input type="radio" name="charc" value="3" (click)="classChoice(2)"> Berserker
-                    <input type="radio" name="charc" value="4" (click)="classChoice(3)"> Chieftain
-                    <input type="radio" name="charc" value="4" (click)="classChoice(4)"> Rogue
-                    <input type="radio" name="charc" value="5" (click)="classChoice(5)"> Trickster
-                    <input type="radio" name="charc" value="6" (click)="classChoice(6)"> Mage
-                    <input type="radio" name="charc" value="7" (click)="classChoice(7)"> Wizard
-                    <input type="radio" name="charc" value="8" (click)="classChoice(8)"> Alchemist
+                    <input type="radio" name="charc" value="2" (click)="classChoice(1)" checked> Adventurer
                 </fieldset>`,
 })
 
